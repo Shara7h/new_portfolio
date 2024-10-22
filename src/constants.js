b7d7a6cccf889d4303e0f3a2219b21b05bd89b1c
Sharath <sharathkulal373@gmail.com>
2024-10-22 21:11:53 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -10,15 +10,15 @@ export const name = 'Sharath';
 
 export const experiences = [
   {
-    company: '....',
-    role: '.......',
-    duration: 'April 2023 - Present',
+    company: 'UniAthena',
+    role: 'Basic Of Data Science',
+    duration: '08 Oct 2024',
     logo: 'https://images.crunchbase.com/image/upload/c_pad,f_auto,q_auto:eco,dpr_1/e5gz1yggdy8sc1umna3e',
     points: [
       <img src="19.png" alt="description of image" />,
       <div style={{ marginTop: '10px', textAlign: 'center' }}>
         <button
-          onClick={() => window.location.href = 'https://yourverificationpage.com'}
+          onClick={() => window.location.href = 'https://docs.uniathena.com/prod/course/certificate/117_1728405104_certificate.jpg'}
           style={{
             backgroundColor: '#1d1836',
             color: 'white',
@@ -42,15 +42,15 @@ export const experiences = [
     ],
     url: 'https://inncircles.com/#home'
   },{
-    company: '....',
-    role: '.......',
-    duration: 'April 2023 - Present',
+    company: 'UniAthena',
+    role: 'Basic of Python',
+    duration: '03 Oct 2024',
     logo: 'https://images.crunchbase.com/image/upload/c_pad,f_auto,q_auto:eco,dpr_1/e5gz1yggdy8sc1umna3e',
     points: [
       <img src="18.png" alt="description of image" />,
       <div style={{ marginTop: '10px', textAlign: 'center' }}>
         <button
-          onClick={() => window.location.href = 'https://yourverificationpage.com'}
+          onClick={() => window.location.href = 'https://docs.uniathena.com/prod/course/certificate/323_1727932682_certificate.jpg'}
           style={{
             backgroundColor: '#1d1836',
             color: 'white',
@@ -73,16 +73,17 @@ export const experiences = [
       </div>
     ],
     url: 'https://inncircles.com/#home'
-  },{
-    company: '....',
-    role: '.......',
-    duration: 'April 2023 - Present',
+  },
+  {
+    company: 'Great Learning',
+    role: 'Face Detection with OpenCV in Python',
+    duration: 'October 2024',
     logo: 'https://images.crunchbase.com/image/upload/c_pad,f_auto,q_auto:eco,dpr_1/e5gz1yggdy8sc1umna3e',
     points: [
       <img src="17.jpg" alt="description of image" />,
       <div style={{ marginTop: '10px', textAlign: 'center' }}>
         <button
-          onClick={() => window.location.href = 'https://yourverificationpage.com'}
+          onClick={() => window.location.href = 'https://www.mygreatlearning.com/certificate/STMXLISV'}
           style={{
             backgroundColor: '#1d1836',
             color: 'white',
@@ -107,15 +108,15 @@ export const experiences = [
     url: 'https://inncircles.com/#home'
   },
   {
-    company: '....',
-    role: '.......',
-    duration: 'April 2023 - Present',
+    company: 'Great Learning',
+    role: 'Face Detection with OpenCV in Python',
+    duration: 'October 2024',
     logo: 'https://images.crunchbase.com/image/upload/c_pad,f_auto,q_auto:eco,dpr_1/e5gz1yggdy8sc1umna3e',
     points: [
-      <img src="16.png" alt="description of image" />,
+      <img src="17.jpg" alt="description of image" />,
       <div style={{ marginTop: '10px', textAlign: 'center' }}>
         <button
-          onClick={() => window.location.href = 'https://yourverificationpage.com'}
+          onClick={() => window.location.href = 'https://www.mygreatlearning.com/certificate/STMXLISV'}
           style={{
             backgroundColor: '#1d1836',
             color: 'white',
@@ -140,15 +141,48 @@ export const experiences = [
     url: 'https://inncircles.com/#home'
   },
   {
-    company: '....',
-    role: '.......',
-    duration: 'April 2023 - Present',
+    company: 'Simpleilearn',
+    role: 'Business Analytics  with Excel',
+    duration: 'September 21,2024',
+    logo: 'https://images.crunchbase.com/image/upload/c_pad,f_auto,q_auto:eco,dpr_1/e5gz1yggdy8sc1umna3e',
+    points: [
+      <img src="116.png" alt="description of image" />,
+      <div style={{ marginTop: '10px', textAlign: 'center' }}>
+        <button
+          onClick={() => window.location.href = 'https://www.guvi.in/share-certificate/31bJ5721H4a7982f58'}
+          style={{
+            backgroundColor: '#1d1836',
+            color: 'white',
+            border: 'none',
+            padding: '10px 20px',
+            cursor: 'pointer',
+            fontSize: '16px',
+            borderRadius: '5px',
+            boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)',
+            transition: '0.3s',
+            textTransform: 'uppercase',
+            letterSpacing: '1px',
+            margin: '0 auto'
+          }}
+          onMouseOver={(e) => e.currentTarget.style.backgroundColor = '#333'}
+          onMouseOut={(e) => e.currentTarget.style.backgroundColor = '#1d1836'}
+        >
+          Verify
+        </button>
+      </div>
+    ],
+    url: 'https://inncircles.com/#home'
+  },
+  {
+    company: 'Udemy',
+    role: 'R Programming',
+    duration: 'Sept. 8,2024',
     logo: 'https://images.crunchbase.com/image/upload/c_pad,f_auto,q_auto:eco,dpr_1/e5gz1yggdy8sc1umna3e',
     points: [
       <img src="15.jpg" alt="description of image" />,
       <div style={{ marginTop: '10px', textAlign: 'center' }}>
         <button
-          onClick={() => window.location.href = 'https://yourverificationpage.com'}
+          onClick={() => window.location.href = 'https://www.udemy.com/certificate/UC-0da8d366-34cd-489a-aac1-8a7c5249f42e/'}
           style={{
             backgroundColor: '#1d1836',
             color: 'white',
@@ -173,15 +207,15 @@ export const experiences = [
     url: 'https://inncircles.com/#home'
   },
   {
-    company: '....',
-    role: '.......',
-    duration: 'April 2023 - Present',
+    company: 'Eduonix',
+    role: 'Learn HTML5 D and D Image Uploader',
+    duration: 'September 17,2024',
     logo: 'https://images.crunchbase.com/image/upload/c_pad,f_auto,q_auto:eco,dpr_1/e5gz1yggdy8sc1umna3e',
     points: [
       <img src="14.jpeg" alt="description of image" />,
       <div style={{ marginTop: '10px', textAlign: 'center' }}>
         <button
-          onClick={() => window.location.href = 'https://yourverificationpage.com'}
+          onClick={() => window.location.href = 'https://www.eduonix.com/certificate/6ad81a6eb4'}
           style={{
             backgroundColor: '#1d1836',
             color: 'white',
@@ -205,15 +239,15 @@ export const experiences = [
     ],
     url: 'https://inncircles.com/#home'
   },{
-    company: '....',
-    role: '.......',
-    duration: 'April 2023 - Present',
+    company: 'Eduonix',
+    role: 'R Programming For Absolute Beginners',
+    duration: 'September 11,2024',
     logo: 'https://images.crunchbase.com/image/upload/c_pad,f_auto,q_auto:eco,dpr_1/e5gz1yggdy8sc1umna3e',
     points: [
       <img src="13.jpeg" alt="description of image" />,
       <div style={{ marginTop: '10px', textAlign: 'center' }}>
         <button
-          onClick={() => window.location.href = 'https://yourverificationpage.com'}
+          onClick={() => window.location.href = 'https://www.eduonix.com/certificate/b624d26234'}
           style={{
             backgroundColor: '#1d1836',
             color: 'white',
@@ -237,15 +271,15 @@ export const experiences = [
     ],
     url: 'https://inncircles.com/#home'
   },{
-    company: '....',
-    role: '.......',
-    duration: 'April 2023 - Present',
+    company: 'Eduonix',
+    role: 'WordPress Plugin Development',
+    duration: 'September 14,2024',
     logo: 'https://images.crunchbase.com/image/upload/c_pad,f_auto,q_auto:eco,dpr_1/e5gz1yggdy8sc1umna3e',
     points: [
       <img src="12.jpeg" alt="description of image" />,
       <div style={{ marginTop: '10px', textAlign: 'center' }}>
         <button
-          onClick={() => window.location.href = 'https://yourverificationpage.com'}
+          onClick={() => window.location.href = 'https://www.eduonix.com/certificate/809a5520e0'}
           style={{
             backgroundColor: '#1d1836',
             color: 'white',
@@ -269,15 +303,15 @@ export const experiences = [
     ],
     url: 'https://inncircles.com/#home'
   },{
-    company: '....',
-    role: '.......',
-    duration: 'April 2023 - Present',
+    company: 'Udemy',
+    role: 'CSS Crash Course For Beginners',
+    duration: 'Aug. 1,2024',
     logo: 'https://images.crunchbase.com/image/upload/c_pad,f_auto,q_auto:eco,dpr_1/e5gz1yggdy8sc1umna3e',
     points: [
       <img src="11.jpg" alt="description of image" />,
       <div style={{ marginTop: '10px', textAlign: 'center' }}>
         <button
-          onClick={() => window.location.href = 'https://yourverificationpage.com'}
+          onClick={() => window.location.href = 'https://www.udemy.com/certificate/UC-1287c0de-3184-4b5d-8f8f-ea7909f903e5/'}
           style={{
             backgroundColor: '#1d1836',
             color: 'white',
@@ -301,15 +335,15 @@ export const experiences = [
     ],
     url: 'https://inncircles.com/#home'
   },{
-    company: '....',
-    role: '.......',
-    duration: 'April 2023 - Present',
+    company: 'Udemy',
+    role: 'Excel files with Python',
+    duration: 'Aug. 10,2024',
     logo: 'https://images.crunchbase.com/image/upload/c_pad,f_auto,q_auto:eco,dpr_1/e5gz1yggdy8sc1umna3e',
     points: [
       <img src="10.jpg" alt="description of image" />,
       <div style={{ marginTop: '10px', textAlign: 'center' }}>
         <button
-          onClick={() => window.location.href = 'https://yourverificationpage.com'}
+          onClick={() => window.location.href = 'https://www.udemy.com/certificate/UC-2b855052-e7a2-4358-ada0-57ccd4286e84/'}
           style={{
             backgroundColor: '#1d1836',
             color: 'white',
@@ -333,15 +367,15 @@ export const experiences = [
     ],
     url: 'https://inncircles.com/#home'
   },{
-    company: '....',
-    role: 'HTML,CSS,JS',
-    duration: 'April 2023 - Present',
+    company: 'Udemy',
+    role: 'Practical HTML,CSS,JS',
+    duration: 'Aug. 1,2024',
     logo: 'https://images.crunchbase.com/image/upload/c_pad,f_auto,q_auto:eco,dpr_1/e5gz1yggdy8sc1umna3e',
     points: [
       <img src="9.jpg" alt="description of image" />,
       <div style={{ marginTop: '10px', textAlign: 'center' }}>
         <button
-          onClick={() => window.location.href = 'https://yourverificationpage.com'}
+          onClick={() => window.location.href = 'https://www.udemy.com/certificate/UC-dfbd3f90-664f-42c7-a3a5-105fe5a7b48a/'}
           style={{
             backgroundColor: '#1d1836',
             color: 'white',
@@ -367,13 +401,13 @@ export const experiences = [
   },{
     company: 'Udemy',
     role: 'MS Word ',
-    duration: 'April 2023 - Present',
+    duration: 'July 16,2024',
     logo: 'https://images.crunchbase.com/image/upload/c_pad,f_auto,q_auto:eco,dpr_1/e5gz1yggdy8sc1umna3e',
     points: [
       <img src="8.jpg" alt="description of image" />,
       <div style={{ marginTop: '10px', textAlign: 'center' }}>
         <button
-          onClick={() => window.location.href = 'https://yourverificationpage.com'}
+          onClick={() => window.location.href = 'https://www.udemy.com/certificate/UC-59f15a6b-49f6-4253-b4df-630be5df0241/'}
           style={{
             backgroundColor: '#1d1836',
             color: 'white',
@@ -399,13 +433,13 @@ export const experiences = [
   },{
     company: 'Udemy',
     role: 'Ethicall Hack',
-    duration: 'April 2023 - Present',
+    duration: 'June 7,2024',
     logo: 'https://images.crunchbase.com/image/upload/c_pad,f_auto,q_auto:eco,dpr_1/e5gz1yggdy8sc1umna3e',
     points: [
       <img src="7.jpg" alt="description of image" />,
       <div style={{ marginTop: '10px', textAlign: 'center' }}>
         <button
-          onClick={() => window.location.href = 'https://yourverificationpage.com'}
+          onClick={() => window.location.href = 'https://www.udemy.com/certificate/UC-d151df1b-4c14-49e8-b3a8-fa7ffac98efc/'}
           style={{
             backgroundColor: '#1d1836',
             color: 'white',
@@ -431,13 +465,13 @@ export const experiences = [
   },{
     company: 'Udemy',
     role: 'Advance Python Programming',
-    duration: 'April 2023 - Present',
+    duration: 'June 24,2024',
     logo: 'https://images.crunchbase.com/image/upload/c_pad,f_auto,q_auto:eco,dpr_1/e5gz1yggdy8sc1umna3e',
     points: [
       <img src="6.jpg" alt="description of image" />,
       <div style={{ marginTop: '10px', textAlign: 'center' }}>
         <button
-          onClick={() => window.location.href = 'https://yourverificationpage.com'}
+          onClick={() => window.location.href = 'https://www.udemy.com/certificate/UC-bb5a2e6f-e156-4d9c-99f3-27acddd6f81a/'}
           style={{
             backgroundColor: '#1d1836',
             color: 'white',
@@ -463,14 +497,14 @@ export const experiences = [
   },
   {
     company: 'Udemy',
-    role: 'Python Demonstrastion',
-    duration: 'April 2023 - Present',
+    role: 'Python Demonstration',
+    duration: 'June 24,2024',
     logo: 'https://images.crunchbase.com/image/upload/c_pad,f_auto,q_auto:eco,dpr_1/e5gz1yggdy8sc1umna3e',
     points: [
       <img src="5.jpg" alt="description of image" />,
       <div style={{ marginTop: '10px', textAlign: 'center' }}>
         <button
-          onClick={() => window.location.href = 'https://yourverificationpage.com'}
+          onClick={() => window.location.href = 'https://www.udemy.com/certificate/UC-fe440338-09d5-40f0-9b7c-cfffeb19bce4/'}
           style={{
             backgroundColor: '#1d1836',
             color: 'white',
@@ -496,13 +530,13 @@ export const experiences = [
   },{
     company: 'Udemy',
     role: 'Ethically Hacking',
-    duration: 'April 2023 - Present',
+    duration: 'April 6,2024',
     logo: 'https://images.crunchbase.com/image/upload/c_pad,f_auto,q_auto:eco,dpr_1/e5gz1yggdy8sc1umna3e',
     points: [
       <img src="4.jpg" alt="description of image" />,
       <div style={{ marginTop: '10px', textAlign: 'center' }}>
         <button
-          onClick={() => window.location.href = 'https://yourverificationpage.com'}
+          onClick={() => window.location.href = 'https://www.udemy.com/certificate/UC-11b1cac2-78a8-4647-b386-8badaa18bb7c/'}
           style={{
             backgroundColor: '#1d1836',
             color: 'white',
@@ -529,13 +563,13 @@ export const experiences = [
   {
     company: 'Udemy',
     role: 'Ethical Hacking',
-    duration: 'April 2023 - Present',
+    duration: 'April 11,2024',
     logo: 'https://images.crunchbase.com/image/upload/c_pad,f_auto,q_auto:eco,dpr_1/e5gz1yggdy8sc1umna3e',
     points: [
       <img src="3.jpg" alt="description of image" />,
       <div style={{ marginTop: '10px', textAlign: 'center' }}>
         <button
-          onClick={() => window.location.href = 'https://yourverificationpage.com'}
+          onClick={() => window.location.href = 'https://www.udemy.com/certificate/UC-7d1d6ae7-7ba5-4275-ad28-a02a4e392e9f/'}
           style={{
             backgroundColor: '#1d1836',
             color: 'white',
@@ -560,14 +594,14 @@ export const experiences = [
     url: 'https://inncircles.com/#home'
   },{
     company: 'Udemy',
-    role: 'Proffessional Diploms in Python ',
-    duration: 'April 2023 - Present',
+    role: 'Professional Diploma in Python ',
+    duration: 'April 6,2024',
     logo: 'https://images.crunchbase.com/image/upload/c_pad,f_auto,q_auto:eco,dpr_1/e5gz1yggdy8sc1umna3e',
     points: [
       <img src="2.jpg" alt="description of image" />,
       <div style={{ marginTop: '10px', textAlign: 'center' }}>
         <button
-          onClick={() => window.location.href = 'https://yourverificationpage.com'}
+          onClick={() => window.location.href = 'https://www.udemy.com/certificate/UC-8cf0896c-91ff-4f51-a420-1d5647f2872b/'}
           style={{
             backgroundColor: '#1d1836',
             color: 'white',
@@ -594,7 +628,7 @@ export const experiences = [
   {
     company: 'Udemy',
     role: 'Python For Beginers',
-    duration: 'April 2023 - Present',
+    duration: 'March 17,2024',
     logo: 'https://images.crunchbase.com/image/upload/c_pad,f_auto,q_auto:eco,dpr_1/e5gz1yggdy8sc1umna3e',
     points: [
       <img src="1.jpg" alt="description of image" />,
