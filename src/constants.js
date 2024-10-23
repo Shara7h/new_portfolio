78348796c5a887e2db622b93db186db88992af71
Sharath <sharathkulal373@gmail.com>
2024-10-23 20:30:38 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -12,7 +12,7 @@ export const experiences = [
    {
     company: 'Simpleilearn',
     role: 'Business Analytics  with Excel',
-    duration: '29th aug,2024',
+    duration: '23rd Oct,2024',
     logo: 'https://shara7h.github.io/1/sil.jpg',
     points: [
       <img src="21.png" alt="description of image" />,
