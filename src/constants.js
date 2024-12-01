daa9289940d87717f61532a7bd8bf2bd115c926e
Sharath <sharathkulal373@gmail.com>
2024-12-01 22:09:31 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -664,6 +664,3 @@ export const experiences = [
 export const EMAIL_JS_SERVICE_ID = process.env.REACT_APP_EMAIL_JS_SERVICE_ID;
 export const EMAIL_JS_TEMPLATE_ID = process.env.REACT_APP_EMAIL_JS_TEMPLATE_ID;
 export const EMAIL_JS_PUBLIC_KEY = process.env.REACT_APP_EMAIL_JS_PUBLIC_KEY;
-0 commit comments
-Comments
-0
